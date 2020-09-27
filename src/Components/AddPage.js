7ddcc7b253e20df947a818fd4743fac238315676
fisnikajredini <fisnikajredini1995@outlook.com>
2020-09-28 01:07:22 +0200

    First commit

---
+++ src/Components/AddPage.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react'
+
+export class AddPage extends Component {
+    render() {
+        return (
+            <div>
+                <h1>Add / Dodaj</h1>
+            </div>
+        )
+    }
+}
+
+export default AddPage
