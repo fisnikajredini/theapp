8434aa327532b7c6b7d828d778329a3d3a6f9b76
fisnikajredini <fisnikajredini1995@outlook.com>
2020-10-08 22:12:54 +0200

    Fisnik

--- frontend/src/Components/AddCompany.js
+++ frontend/src/Components/AddCompany.js
@@ -1,13 +1,35 @@
 import React, { Component } from 'react'
+import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
 
 export class AddCompany extends Component {
     render() {
         return (
             <div>
-                <h1>Add Company Page</h1>
+                <MuiThemeProvider>
+                <React.Fragment> 
+                        <h2 
+                        style={styles.subheading}
+                        >Додај</h2>
+                        <br/>
+                </React.Fragment>
+                </MuiThemeProvider>
             </div>
         )
     }
+}
+
+const styles ={
+    button: {
+        margin: 15,
+    },
+    banner:{
+        backgroundColor: "#3257a8"
+    },
+    subheading: {
+        color: "rgb(136, 136, 136)",
+        fontWeight: "600"
+    },
+    
 }
 
 export default AddCompany
