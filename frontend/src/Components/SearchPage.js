a83f638056b39f4c484d764fab25e092010b6440
fisnikajredini <fisnikajredini1995@outlook.com>
2020-10-16 22:54:50 +0200

    adjusting design of search

--- frontend/src/Components/SearchPage.js
+++ frontend/src/Components/SearchPage.js
@@ -1,183 +1,91 @@
-import React, { useState, useEffect, FC } from "react";
-import {
-  GridComponent,
-  ColumnDirective,
-  ColumnsDirective,
-  Page,
-  Filter,
-  Inject,
-  FilterSettingsModel,
-  EditSettingsModel,
-  ToolbarItems,
-  Grid,
-  Toolbar,
-  PdfExport,
-  ExcelExport,
-  dataSourceChanged,
-} from "@syncfusion/ej2-react-grids";
-import data from "./dataSource.json";
-import "./searchtable.css";
+import * as React from 'react';
+import * as ReactDOM from 'react-dom';
+import { Grid, GridColumn as Column, GridToolbar } from '@progress/kendo-react-grid';
+import { GridPDFExport } from '@progress/kendo-react-pdf';
+import { sampleProducts } from './dataSource.jsx';
 import axios from "axios";
 
+
 export default class SearchPage extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      dataCategory: null,
-    };
-  }
-  componentDidMount = () => {
-    axios.get("/getcategory").then((res) => {
-        console.log(res.data.data)
-        let readedData = res.data.data
-      this.setState({
-          dataCategory:readedData
-      })
-      console.log(this.state.dataCategory);
-    });
-    
-  };
-  render() {
-    const filterSettings = {
-      ignoreAccent: true,
-    };
-    const editOptions: EditSettingsModel = {allowDeleting: true}
-    const toolbarOptions: ToolbarItems[] = ['PdfExport', 'ExcelExport', 'Delete'];
+    gridPDFExport;
+    total = sampleProducts.length;
+    pageSize = 15;
 
-    let grid: Grid | null;
-    const toolbarClick = (args: any) => {
-      if (grid) {
-        if (args.item.id.includes("pdfexport")) {
-          grid.pdfExport({
-            fileName: "Delovodnik.pdf",
-            header: {
-              fromTop: 0,
-              height: 130,
-              contents: [
-                {
-                  type: "Text",
-                  value: "Era Consulting",
-                  position: { x: 0, y: 50 },
-                  style: { textBrushColor: "#00000", fontSize: 13 },
-                },
-              ],
-            },
-            footer: {
-              contents: [
-                {
-                  type: "Text",
-                  value: "Exported from Delovodnik Application",
-                  position: { x: 0, y: 50 },
-                  style: { textBrushColor: "#00000", fontSize: 13 },
-                },
-              ],
-              fromBottom: 130,
-              height: 130,
-            },
-          });
-        } else if (args.item.id.includes("excelexport")) {
-          grid.excelExport({
-            fileName: "delovodnik.xlsx",
-            header: {
-              headerRows: 1,
-              rows: [
-                {
-                  cells: [
-                    {
-                      colSpan: 6,
-                      value: "Era Consulting",
-                      style: {
-                        fontColor: "#3257a8",
-                        fontSize: 20,
-                        hAlign: "Center",
-                        bold: true,
-                      },
-                    },
-                  ],
-                },
-              ],
-            },
-            footer: {
-              footerRows: 1,
-              rows: [
-                {
-                  cells: [
-                    {
-                      colSpan: 6,
-                      value: "Exported from Delovodnik Application",
-                      style: { hAlign: "Center", bold: true },
-                    },
-                  ],
-                },
-              ],
-            },
-          });
-        }
-      }
+    state = {
+        data: sampleProducts.slice(0, this.pageSize),
+        skip: 0,
+        exporting: false
     };
 
+    render() {
+        const grid = (
+            <Grid
+                data={this.state.data}
+                pageable={true}
+                onPageChange={this.pageChange}
+                pageSize={this.pageSize}
+                total={this.total}
+                skip={this.state.skip}
+            >
+                <GridToolbar>
+                    <button
+                        title="Export PDF"
+                        className="k-button k-primary"
+                        onClick={this.exportPDF}
+                        disabled={this.state.exporting}
+                    >
+                        Export PDF
+                    </button>
+                </GridToolbar>
+                <Column field="ProductID" title="ID" width="50px" />
+                <Column field="ProductName" title="Name" width="300px" />
+                <Column field="Category.CategoryName" title="Category" />
+                <Column field="UnitPrice" title="Price" />
+                <Column field="UnitsInStock" title="In stock" />
+                <Column field="Discontinued" title="Discontinued" />
+            </Grid>
+        );
 
-    function dataSourceChanged(state) {
-      console.log(state);
+        return (
+          <div style={{ margin: "2%", marginTop: "3%", marginBottom: "5%" }}>
+          <h2 style={styles.subheading}>Побарај Деловодник</h2>
+          <br />
+                {grid}
+                <GridPDFExport
+                    ref={pdfExport => this.gridPDFExport = pdfExport}
+                >
+                    {grid}
+                </GridPDFExport>
+            </div>
+        );
     }
 
+    pageChange = (event) => {
+        this.setState(this.createState(event.page.skip, event.page.take));
+    }
 
-    return (
-      <div style={{ margin: "2%", marginTop: "3%", marginBottom: "5%" }}>
-        <h2 style={styles.subheading}>Побарај Деловодник</h2>
-        <br />
-        <GridComponent
-          dataSource={this.state.dataCategory}
-          ref={(g) => (grid = g)}
-          allowPaging={true}
-          allowFiltering
-          filterSettings={filterSettings}
-          pageSettings={{ pageSize: 20 }}
-          toolbar={toolbarOptions}
-          allowPdfExport={true}
-          allowExcelExport
-          toolbarClick={toolbarClick}
-          editSettings={editOptions}
-          dataSourceChanged={dataSourceChanged}
-        >
-          <ColumnsDirective>
-            { /*<ColumnDirective
-              field="_id"
-              headerText="ID"
-              textAlign="Right"
-              width="100"
-              isPrimaryKey={true}
-            /> */}
-            <ColumnDirective
-              field="company_name"
-              headerText="Фирма"
-              textAlign="Left"
-              width="250"
-            />
-            <ColumnDirective field="category" headerText="Категорија" textAlign="Left" width="250" />
-            <ColumnDirective field="subCategory" headerText="Под-Категорија" textAlign="Left" width="500" />
-            <ColumnDirective
-              field="date"
-              headerText="Датум"
-              textAlign="Left"
-              width="130"
-            />
-            <ColumnDirective
-              field="Life"
-              headerText="Рок на чување"
-              textAlign="Left"
-              width="150"
-            />
-          </ColumnsDirective>
-          <Inject services={[Page, Filter, Toolbar, PdfExport, ExcelExport]} />
-        </GridComponent>
-      </div>
-    );
-  }
+    createState(skip, take) {
+        return {
+            data: sampleProducts.slice(skip, skip + take),
+            skip: skip
+        };
+    }
+
+    exportPDF = () => {
+        this.gridPDFExport.save(this.state.data, this.pdfExportDone);
+
+        this.setState({ exporting: true });
+    }
+
+    pdfExportDone = () => {
+        this.setState({ exporting: false });
+    }
 }
+
 const styles = {
   subheading: {
     color: "rgb(136, 136, 136)",
     fontWeight: "600",
   },
 };
+
--- frontend/src/Components/testing.js
+++ frontend/src/Components/testing.js
@@ -1,128 +1,183 @@
-import React, { Component } from 'react';
-import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
-import Container from '@material-ui/core/Container';
-import RaisedButton from "material-ui/RaisedButton";
-import { DropDownList } from "@progress/kendo-react-dropdowns";
-import { dataCategories, dataSubcat } from "./data.js";
-import "./dropdown.css";
-import Grid from '@material-ui/core/Grid';
+import React, { useState, useEffect, FC } from "react";
+import {
+  GridComponent,
+  ColumnDirective,
+  ColumnsDirective,
+  Page,
+  Filter,
+  Inject,
+  FilterSettingsModel,
+  EditSettingsModel,
+  ToolbarItems,
+  Grid,
+  Toolbar,
+  PdfExport,
+  ExcelExport,
+  dataSourceChanged,
+} from "@syncfusion/ej2-react-grids";
+import data from "./dataSource";
+import "./searchtable.css";
+import axios from "axios";
 
-const defaultItemCompany = { company_name: "Селектирај Фирма ..." }; // Emrin e kopanis ktau de duet ta mare pi te tabela e 2 
-const defaultItemCategory = { categoryName: "Селектирај Категорија ..." }; // Njejt ene ktau pi te tabela e 2 
-const defaultItemSubcat = { subcatName: "Селектирај Под-Категорија ..." }; // Njejt ene ktau pi te tabela e 2 
+export default class SearchPage extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      dataCategory: null,
+    };
+  }
+  componentDidMount = () => {
+    axios.get("/getcategory").then((res) => {
+        console.log(res.data.data)
+        let readedData = res.data.data
+      this.setState({
+          dataCategory:readedData
+      })
+      console.log(this.state.dataCategory);
+    });
+    
+  };
+  render() {
+    const filterSettings = {
+      ignoreAccent: true,
+    };
+    const editOptions: EditSettingsModel = {allowDeleting: true}
+    const toolbarOptions: ToolbarItems[] = ['PdfExport', 'ExcelExport', 'Delete'];
 
-class SearchPage extends Component {
-    constructor(props){
-        super(props)
-        this.state = {
-            category: null,
-            subcat: null,
-            subcats: dataSubcat,
-          };
+    let grid: Grid | null;
+    const toolbarClick = (args: any) => {
+      if (grid) {
+        if (args.item.id.includes("pdfexport")) {
+          grid.pdfExport({
+            fileName: "Delovodnik.pdf",
+            header: {
+              fromTop: 0,
+              height: 130,
+              contents: [
+                {
+                  type: "Text",
+                  value: "Era Consulting",
+                  position: { x: 0, y: 50 },
+                  style: { textBrushColor: "#00000", fontSize: 13 },
+                },
+              ],
+            },
+            footer: {
+              contents: [
+                {
+                  type: "Text",
+                  value: "Exported from Delovodnik Application",
+                  position: { x: 0, y: 50 },
+                  style: { textBrushColor: "#00000", fontSize: 13 },
+                },
+              ],
+              fromBottom: 130,
+              height: 130,
+            },
+          });
+        } else if (args.item.id.includes("excelexport")) {
+          grid.excelExport({
+            fileName: "delovodnik.xlsx",
+            header: {
+              headerRows: 1,
+              rows: [
+                {
+                  cells: [
+                    {
+                      colSpan: 6,
+                      value: "Era Consulting",
+                      style: {
+                        fontColor: "#3257a8",
+                        fontSize: 20,
+                        hAlign: "Center",
+                        bold: true,
+                      },
+                    },
+                  ],
+                },
+              ],
+            },
+            footer: {
+              footerRows: 1,
+              rows: [
+                {
+                  cells: [
+                    {
+                      colSpan: 6,
+                      value: "Exported from Delovodnik Application",
+                      style: { hAlign: "Center", bold: true },
+                    },
+                  ],
+                },
+              ],
+            },
+          });
+        }
+      }
+    };
+
+
+    function dataSourceChanged(state) {
+      console.log(state);
     }
 
-    categoryChange = (event) => {
-        const category = event.target.value;
-        const subcats = dataSubcat.filter(
-          (subcat) => subcat.categoryId === category.categoryId
-        );
-    
-        this.setState({
-          category: category,
-          subcats: subcats,
-          subcat: null,
-          name: null,
-        });
-      };
-    
-      subcatChange = (event) => {
-        const subcat = event.target.value;
-    
-        this.setState({
-          subcat: subcat,
-        });
-      };
-    
-      render() {
-        const category = this.state.category;
-        const subcat = this.state.subcat;
-    
-        const hasCategory = category && category !== defaultItemCategory;
-        const hasSubcat = subcat && subcat !== defaultItemSubcat;
 
-        return (
-            <div>
-                <MuiThemeProvider>
-                <React.Fragment> 
-                    <h2 
-                    style={styles.subheading}
-                    >Побарај Деловодник</h2>
-                    <br/>
-                    <Container maxWidth="lg">
-                        <Grid className="searchpage-dd" container spacing={3} style={styles.fgrids}>
-                            <Grid item xs={4} sm={4}>
-                            <div style={{ display: "inline-block" }}>
-                                <DropDownList
-                                data={this.props.companyNames}
-                                textField="company_name"
-                                //onChange={this.categoryChange}
-                                defaultItem={defaultItemCompany}
-                                // value={data}
-                                />
-                            </div>
-                            </Grid>
-                            <Grid item xs={4} sm={4}>
-                            <div style={{ display: "inline-block" }}>
-                                <DropDownList
-                                data={dataCategories}
-                                textField="categoryName"
-                                onChange={this.categoryChange}
-                                defaultItem={defaultItemCategory}
-                                //value={category}
-                                />
-                            </div>
-                            </Grid>
-                            <Grid item xs={4} sm={4}>
-                            <div style={{ display: "inline-block" }}>
-                                <DropDownList
-                                disabled={!hasCategory}
-                                data={this.state.subcats}
-                                textField="subcatName"
-                                onChange={this.subcatChange}
-                                defaultItem={defaultItemSubcat}
-                                value={subcat}
-                                />
-                            </div>
-                            </Grid>
-                        </Grid>
-                        <br/>
-                            <RaisedButton
-                            label="Побарај"
-                            id="button-search"
-                            primary={true}
-                            style={styles.button}
-                            />
-                    </Container>
-                </React.Fragment>
-                </MuiThemeProvider>
-            </div>
-        )
-    }
+    return (
+      <div style={{ margin: "2%", marginTop: "3%", marginBottom: "5%" }}>
+        <h2 style={styles.subheading}>Побарај Деловодник</h2>
+        <br />
+        <GridComponent
+          dataSource={this.state.dataCategory}
+          ref={(g) => (grid = g)}
+          allowPaging={true}
+          allowFiltering
+          filterSettings={filterSettings}
+          pageSettings={{ pageSize: 20 }}
+          toolbar={toolbarOptions}
+          allowPdfExport={true}
+          allowExcelExport
+          toolbarClick={toolbarClick}
+          editSettings={editOptions}
+          dataSourceChanged={dataSourceChanged}
+        >
+          <ColumnsDirective>
+            { /*<ColumnDirective
+              field="_id"
+              headerText="ID"
+              textAlign="Right"
+              width="100"
+              isPrimaryKey={true}
+            /> */}
+            <ColumnDirective
+              field="company_name"
+              headerText="Фирма"
+              textAlign="Left"
+              width="250"
+            />
+            <ColumnDirective field="category" headerText="Категорија" textAlign="Left" width="250" />
+            <ColumnDirective field="subCategory" headerText="Под-Категорија" textAlign="Left" width="500" />
+            <ColumnDirective
+              field="date"
+              headerText="Датум"
+              textAlign="Left"
+              width="130"
+            />
+            <ColumnDirective
+              field="Life"
+              headerText="Рок на чување"
+              textAlign="Left"
+              width="150"
+            />
+          </ColumnsDirective>
+          <Inject services={[Page, Filter, Toolbar, PdfExport, ExcelExport]} />
+        </GridComponent>
+      </div>
+    );
+  }
 }
-
-const styles ={
-    button: {
-        margin: 15,
-    },
-    banner:{
-        backgroundColor: "#3257a8"
-    },
-    subheading: {
-        color: "rgb(136, 136, 136)",
-        fontWeight: "600"
-    },
-    
-}
-document.querySelector("my-app");
-
-export default SearchPage
+const styles = {
+  subheading: {
+    color: "rgb(136, 136, 136)",
+    fontWeight: "600",
+  },
+};
